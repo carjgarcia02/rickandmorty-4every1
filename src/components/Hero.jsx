f321bcb8ca29c5b032a94d0f275e883ae9eee4f9
carjgarcia02 <carjgarcia02@gmail.com>
2023-05-03 19:40:59 -0500

    Hero and Navbar fully revamped

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,36 +2,26 @@ import HeroImage from "../assets/images/hero.jpg";
 
 const Hero = () => {
   return (
-    <section className="hero">
+    <section
+      className="hero"
+      style={{
+        backgroundImage: `url(${HeroImage})`,
+      }}
+    >
       <div className="hero__info">
-        <h1>
-          Welcome to my{" "}
-          <span className="hero__name">
-            <a
-              href="https://en.wikipedia.org/wiki/Rick_and_Morty"
-              target="_blank"
-            >
-              Rick and Morty
-            </a>
-          </span>{" "}
-          app!
-        </h1>
+        <h1>Welcome to my Rick and Morty app!</h1>
         <h2>
           Feel free to look around and <br />
           learn more about the show.
         </h2>
-        <p>Season 7 is coming!</p>
-        <p>
-          <a
-            className="hero__link"
-            href="https://www.inverse.com/entertainment/rick-morty-season-6-episode-7-release-date-time-plot-cast-trailer-for-adult-swims-sci-fi-show"
-            target="_blank"
-          >
-            Latest News
-          </a>
-        </p>
+        <a
+          className="hero__link"
+          href="https://www.inverse.com/entertainment/rick-morty-season-6-episode-7-release-date-time-plot-cast-trailer-for-adult-swims-sci-fi-show"
+          target="_blank"
+        >
+          Read the Latest News
+        </a>
       </div>
-      <img className="hero__image" src={HeroImage} alt="" />
     </section>
   );
 };
--- src/components/characters/CharacterCard.jsx
+++ src/components/characters/CharacterCard.jsx
@@ -2,19 +2,16 @@ const CharacterCard = ({ image, name, status, species, gender }) => {
   return (
     <div className="card">
       <div className="card__header">
-        
+        <div className="status">
+          <div className="status__circle"></div>
+          <p className="status__text">{status}</p>
+        </div>
       </div>
+      <img className="card__image" src={image} alt="Character Image" />
       <div className="card__body">
         <h3 className="card__title">{name}</h3>
-        <img className="card__image" src={image} alt="Character Image" />
         <p className="card__text">
-          <b>Status:</b> {status}
-        </p>
-        <p className="card__text">
-          <b>Species:</b> {species}
-        </p>
-        <p className="card__text">
-          <b>Gender:</b> {gender}
+          {species} - {gender}
         </p>
       </div>
     </div>
